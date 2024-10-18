32686fc7b20ef12a9d977a4e9a141d022703c01b
Zhou RuoHeng <43427057+ryougishiki1999@users.noreply.github.com>
2024-10-18 22:54:08 +0800

    fix some bugs: 1. add more types in gitignore 2. unify the import path and filepath, especially for pycharm

--- src/searchEngine/engine_config.py
+++ src/searchEngine/engine_config.py
@@ -4,10 +4,11 @@ search_engine_dir = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.dirname(search_engine_dir))
 INDEX_DIR = os.path.join(PROJECT_ROOT, 'out', 'indexdir')
 DATA_DIR = os.path.join(PROJECT_ROOT, 'resource', 'data')
+OUT_DIR = os.path.join(PROJECT_ROOT, 'out')
+TMP_DIR = os.path.join(PROJECT_ROOT, 'tmp')
 
 ORIGIN_REVIEW_DATA_PATH = os.path.join(DATA_DIR, 'yelp_academic_dataset_review.json')
 ORIGIN_BUSSIENESS_DATA_PATH = os.path.join(DATA_DIR, 'yelp_academic_dataset_business.json')
 
 REVIEW_DATA_PATH = os.path.join(DATA_DIR, 'review.json')
 BUSSIENESS_DATA_PATH = os.path.join(DATA_DIR, 'business.json')
-
